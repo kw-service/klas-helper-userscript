1f0846987ef428d57fdedb6c5b2abcf9ac40a7c2
nbsp1221 <nbsp1221@gmail.com>
2020-02-15 20:11:11 +0900

    Update version to 1.1.0

--- sources/klas-helper.user.js
+++ sources/klas-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KLAS Helper
 // @namespace    https://github.com/nbsp1221
-// @version      1.0.1
+// @version      1.1.0
 // @description  광운대학교 KLAS 사이트에 편리한 기능을 추가할 수 있는 유저 스크립트
 // @match        https://klas.kw.ac.kr/*
 // @run-at       document-end
