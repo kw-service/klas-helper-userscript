4c103b3f5d9ac973a8effb85cc35992a09e997d2
ssahn0806 <ssahn0806@naver.com>
2020-04-07 13:59:48 +0900

    klas-helper.user.js

--- scripts/klas-helper.user.js
+++ scripts/klas-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KLAS Helper
 // @namespace    https://github.com/nbsp1221
-// @version      1.4.1
+// @version      1.4.0
 // @description  광운대학교 KLAS 사이트에 편리한 기능을 추가할 수 있는 유저 스크립트
 // @match        https://klas.kw.ac.kr/*
 // @run-at       document-end
@@ -245,25 +245,73 @@
 				}
 			});
 		},
-		//이전 학기 석차 조회하기
-		'std/cps/inqire/StandStdPage.do' : () => {
-		  let state=0;
-		       appModule.$watch('stand',function(newval,oldVal){
-		       if(state==0)
-		       {
-			var info = prompt("석차를 조회하고자 하는 년도와 학기를 입력하세요(2016,1) : ").split(",");
-			var params = {"selectYearhakgi":info[0]+','+info[1],"selectChangeYn":"Y"};
-			state++;
-		       }
+		 // 재학 했던 학기의 모든 석차 조회
+		'/std/cps/inqire/StandStdPage.do': () => {
+			// 이전 석차 내역 불러오기
+			$('.tablegw').after(`
+				<div style="margin-top: 10px">
+					<button type="button" class="btn2 btn-learn btn-ranking">이전 석차 내역 불러오기</button>
+				</div>
+			`);
 
-		       if(state==1)
-		       {
-			    appModule.getData(params);
-			    input++;
-		       }
+			$('.btn-ranking').click(() => {
+				// 현재 연도와 학기 및 입학년도 획득
+				let nowYear = appModule.$data.selectYear;
+				let nowSemester = appModule.$data.selectHakgi;
+				let hakbun = appModule.$data.info[0].hakbun;
+				let admissionYear = hakbun.substring(0,4);
+				// 비동기 문제로 타이머 사용
+				const syncTimer = setInterval(() => {
 
+				    if (nowSemester === '2') {
+					nowSemester = '1';
+				    }
+				    else {
+					nowYear--;
+					nowSemester = '2';
+				    }
 
-     });
+					const params = {
+						'selectYearhakgi': nowYear + ',' + nowSemester,
+						'selectChangeYn': 'Y'
+					};
+
+					// 석차 조회
+					axios.post('/std/cps/inqire/StandStdList.do', params).then(function (response) {
+						if (response.data === '') {
+							    if(nowYear < admissionYear)
+							    {
+										 clearInterval(syncTimer);
+
+										 alert('석차 정보를 모두 불러왔습니다.');
+										 $('.btn-ranking').hide();
+							    }
+							    else
+							    {
+								$('table.AType > tbody').append(`
+								 <tr>
+								   <td colspan="8">${nowYear}년도 ${nowSemester}학기의 석차정보가 없습니다.</td>
+								 </tr>
+								`);
+							    }
+						}
+						else {
+							$('table.AType > tbody').append(`
+								<tr>
+									<td>${response.data.thisYear}</td>
+									<td>${response.data.hakgi}</td>
+									<td>${response.data.applyHakjum}</td>
+									<td>${response.data.applySum}</td>
+									<td>${response.data.applyPoint}</td>
+									<td>${response.data.pcnt}</td>
+									<td>${response.data.classOrder} / ${response.data.manNum}</td>
+									<td>${response.data.warningOpt ? response.data.warningOpt : ''}</td>
+								</tr>
+							`);
+						}
+					}.bind(this));
+				}, 500);
+			});
 		},
 		// 온라인 강의 컨텐츠 보기
 		'/std/lis/evltn/OnlineCntntsStdPage.do': () => {
