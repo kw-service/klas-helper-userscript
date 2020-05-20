ee269b2cb7d70a71d0e07a8ef43df56b05fd0e85
retn0 <nbsp1221@gmail.com>
2020-05-20 23:48:14 +0900

    [UPDATE] 온라인 강의 다운로드 이전 방식 제거 (#19)
    온라인 강의 다운로드 기능을 새롭게 변경함에 따라 업데이트 문제로 남겨두
    었던 이전 방식을 제거했습니다.

--- scripts/klas-helper.user.js
+++ scripts/klas-helper.user.js
@@ -19,18 +19,6 @@ function jsCache(filePath) {
 	return filePath + '?v=' + cacheValue;
 }
 
-// Element 생성
-function createElement(elementName, htmlCode) {
-	const newElement = document.createElement(elementName);
-	newElement.innerHTML = htmlCode;
-	return newElement;
-}
-
-// 콘솔에 오류 띄우기
-function consoleError(error, info) {
-	console.error(`[KLAS Helper Error]\n${info.title}: ${info.content}\nMessage: ${error.message}`);
-}
-
 (function () {
 	'use strict';
 
@@ -54,68 +42,6 @@ function consoleError(error, info) {
 // 태그에 삽입되지 않는 함수 목록
 // GM 기능을 사용하기 위해 유저 스크립트 내부의 함수가 필요
 const internalPathFunctions = {
-	// 온라인 강의 컨텐츠 보기
-	// 나중에 없앨 예정입니다. 이 기능은
-	'/std/lis/evltn/OnlineCntntsStdPage.do': () => {
-		// MutationObserver 삽입
-		const observer = new MutationObserver(function (mutationList, observer) {
-			// table 태그에 저장한 고유 번호 파싱
-			const videoCodes = JSON.parse(mutationList[0].target.dataset.videoCodes);
-
-			// 이미 생성된 다운로드 버튼 제거
-			document.querySelectorAll('.btn-download').forEach(function (item) {
-				item.style.display = 'none';
-			});
-
-			// 동영상 XML 정보 획득
-			for (const videoInfo of videoCodes) {
-				GM.xmlHttpRequest({
-					method: 'GET',
-					url: 'https://kwcommons.kw.ac.kr/viewer/ssplayer/uniplayer_support/content.php?content_id=' + videoInfo.videoCode,
-					onload: function (response) {
-						const documentXML = response.responseXML;
-						const videoURLs = [];
-
-						// 분할된 동영상 등 다양한 상황 처리
-						try {
-							if (documentXML.getElementsByTagName('desktop').length > 0) {
-								videoURLs.push(documentXML.getElementsByTagName('media_uri')[0].innerHTML);
-							}
-							else {
-								const mediaURI = documentXML.getElementsByTagName('media_uri')[0].innerHTML;
-
-								for (const videoName of documentXML.getElementsByTagName('main_media')) {
-									videoURLs.push(mediaURI.replace('[MEDIA_FILE]', videoName.innerHTML));
-								}
-							}
-						}
-						catch (error) {
-							consoleError(error, {
-								title: 'Video Code',
-								content: videoInfo.videoCode
-							});
-						}
-
-						// 다운로드 버튼 렌더링
-						videoURLs.forEach((videoURL, i) => {
-							const tdList = document.getElementById('prjctList').querySelectorAll(`tbody > tr:nth-of-type(${videoInfo.index + 1}) > td`);
-							let tdElement = tdList[tdList.length - 1];
-							tdElement = tdElement.className === '' ? tdElement : tdList[tdList.length - 2];
-
-							tdElement.appendChild(createElement('div', `
-								<a href="${videoURL}" target="_blank" style="display: block; margin-top: 10px">
-									<button type="button" class="btn2 btn-gray btn-download">동영상 받기 #${i + 1}</button>
-								</a>
-							`));
-						});
-					}
-				});
-			}
-		});
-
-		// MutationObserver 감지 시작
-		observer.observe(document.querySelector('#prjctList'), { attributes: true });
-	},
 	// 온라인 강의 화면
 	'/spv/lis/lctre/viewer/LctreCntntsViewSpvPage.do': () => {
 		// 온라인 강의 동영상 다운로드
