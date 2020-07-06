2ce8d144616cb65450136bed8ec209247a5fedf1
retn0 <nbsp1221@gmail.com>
2020-07-06 20:02:41 +0900

    [RELEASE] Update dist files

--- dist/main.js
+++ dist/main.js
@@ -9,14 +9,30 @@ function floorFixed(number, decimalPlace = 2) {
     'use strict';
 
     const externalLibs = [
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css',
         'https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js'
     ];
 
     // 외부 라이브러리 삽입
     for (const src of externalLibs) {
-        const scriptElement = document.createElement('script');
-        scriptElement.setAttribute('src', src);
-        document.head.appendChild(scriptElement);
+        let extension = src.split('.');
+        extension = extension[extension.length - 1];
+
+        switch (extension) {
+            case 'js':
+                const scriptElement = document.createElement('script');
+                scriptElement.setAttribute('src', src);
+                document.head.appendChild(scriptElement);
+                break;
+
+            case 'css':
+                const styleElement = document.createElement('link');
+                styleElement.setAttribute('rel', 'stylesheet');
+                styleElement.setAttribute('href', src);
+                document.head.appendChild(styleElement);
+                break;
+        }
     }
 
     // window.onload 시점에 프로그램이 실행되도록 설정
@@ -942,6 +958,49 @@ function routeOnlineLecturePage() {
 
 // 온라인 강의 화면
 function routeTakeLecturePage() {
+    // 단축키 안내 추가
+    (() => {
+        $('body').append(`
+            <div id="modal-keyboard-shortcut" class="modal" style="font-size: 14px">
+                <p><kbd>Enter</kbd> <kbd>F</kbd> : <strong>전체 화면 설정 / 해제</strong></p>
+                <p><kbd>←</kbd> <kbd>→</kbd> : <strong>10초씩 이동</strong></p>
+                <p><kbd>↑</kbd> <kbd>↓</kbd> : <strong>10%씩 볼륨 조절</strong></p>
+                <p><kbd>M</kbd> : <strong>음소거 설정 / 해제</strong></p>
+                <p><kbd>Backspace</kbd> <kbd>P</kbd> : <strong>페이지 단위로 이동 (이전 페이지)</strong></p>
+                <p><kbd>N</kbd> : <strong>페이지 단위로 이동 (다음 페이지)</strong></p>
+                <p><kbd>X</kbd> <kbd>C</kbd> : <strong>0.2 단위로 배속 조절</strong></p>
+                <p><kbd>Z</kbd> : <strong>1.0 배속으로 초기화</strong></p>
+            </div>
+        `);
+
+        $('#modal-keyboard-shortcut kbd').css({
+            backgroundColor: '#eee',
+            border: '1px solid #b4b4b4',
+            borderRadius: '3px',
+            boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2), 0 2px 0 0 rgba(255, 255, 255, 0.7) inset',
+            color: '#333',
+            fontFamily: 'Consolas, monospace',
+            fontSize: '11px',
+            fontWeight: 'bold',
+            padding: '2px 4px',
+            position: 'relative',
+            top: '-1px'
+        });
+
+        $('#modal-keyboard-shortcut strong').css({
+            position: 'relative',
+            top: '1px'
+        });
+
+        $('.mvtopba > label:last-of-type').after(`
+            <label>
+                <a href="#modal-keyboard-shortcut" rel="modal:open" style="background-color: #8e44ad; padding: 10px; text-decoration: none">
+                    <span style="color: white; font-weight: bold; margin-left: 4px">단축키 안내</span>
+                </a>
+            </label>
+        `);
+    })();
+
     // 온라인 강의 고유 번호 파싱
     const videoURL = chkOpen.toString().split('https://kwcommons.kw.ac.kr/em/')[1].split('"')[0];
     document.body.setAttribute('data-video-code', videoURL);
