dac5f58ad7b186bf69e8f5f7ff02c1cf11e87fdc
mirusu400 <mirusu400@naver.com>
2021-10-24 20:59:04 +0900

    [HOTFIX] 타임테이블 시간오류 수정

--- src/routes/timetable.js
+++ src/routes/timetable.js
@@ -2,47 +2,56 @@
  * 페이지 이름: 수업시간표
  * 페이지 주소: https://klas.kw.ac.kr/std/cps/atnlc/TimetableStdPage.do
  */
-const lectureTime = {
-    0: '08:00', 1: '09:00', 2: '10:30', 3: '12:00', 4: '13:30',
-    5: '15:00', 6: '16:30', 7: '18:00', 8: '18:50', 9: '19:40',
-    10: '00:30', 11: '21:20'
+const lectureStartTime = {
+  0: '08:00', 1: '09:00', 2: '10:30', 3: '12:00', 4: '13:30',
+  5: '15:00', 6: '16:30', 7: '18:00', 8: '18:50', 9: '19:40',
+  10: '00:30', 11: '21:20'
+};
+
+const lectureEndTime = {
+  0: '08:50', 1: '10:15', 2: '11:45', 3: '13:15', 4: '14:45',
+  5: '16:15', 6: '17:45', 7: '18:45', 8: '19:35', 9: '20:25',
+  10: '21:15', 11: '22:05'
 };
 const handleTimeTable = () => {
-    const timeTable = appModule.$data.timeTableList;
-    const lectureList = []
+  const timeTable = appModule.$data.timeTableList;
+  const lectureList = [];
 
-    // 원본 소스에서 실제 그려지는 시간표를 파싱합니다
-    for (const item of timeTable) {
-        for (let i = 1; i < 10; i++) {
-            const subjNm = item[`wtSubjNm_${i}`]
-            const subjPrintSeq = item[`wtSubjPrintSeq_${i}`]
-            const startTime = item[`wtTime`] // 시작시간
-            if (subjNm !== undefined) {
-                lectureList.push({
-                    subjNm,
-                    subjPrintSeq,
-                    startTime
-                })
-            }
-        }
+  // 원본 소스에서 실제 그려지는 시간표를 파싱합니다
+  for (const item of timeTable) {
+    for (let i = 1; i < 10; i++) {
+      const subjNm = item[`wtSubjNm_${i}`];
+      const subjPrintSeq = item[`wtSubjPrintSeq_${i}`];
+      const startTime = item[`wtTime`]; // 시작시간
+      if (subjNm !== undefined) {
+        lectureList.push({
+          subjNm,
+          subjPrintSeq,
+          startTime
+        });
+      }
     }
-
-    // 파싱한 데이터를 통해 표에 그려진 값을 찾아가고, 거기에 시간을 추가합니다.
-    for (const item of lectureList) {
-        const subjPrintSeq = item.subjPrintSeq
-        const startTime = item.startTime
-        const className = '.namecol' + String(subjPrintSeq).padStart(2,'0');
-        const endTime = startTime + parseInt($(className).attr('class').split('lessontime')[1]);
-        const currentLectureTime = parseInt($(className).closest('tr').eq(0).find('.time').eq(0).text());
-
-        // 이미 시간이 그려진 강의이거나, 해당되는 강의 시간이 아니라면 그냥 넘어갑니다.
-        if ($(className).find("span").length > 0 || currentLectureTime !== startTime) {
-            continue
-        }
-        $(className).append(
-            `<span class="time">${lectureTime[startTime]} ~ ${lectureTime[endTime]}</span>`
-        );
-    }
+  }
+  // 파싱한 데이터를 통해 표에 그려진 값을 찾아가고, 거기에 시간을 추가합니다.
+  for (const item of lectureList) {
+    const subjPrintSeq = item.subjPrintSeq;
+    const startTime = item.startTime;
+    const className = '.namecol' + String(subjPrintSeq).padStart(2,'0');
+        
+    console.log(item);
+    $(className).each(function(idx, element) {
+      const endTime = startTime + parseInt($(element).attr('class').split('lessontime')[1]) - 1;
+      const currentLectureTime = parseInt($(element).closest('tr').eq(0).find('.time').eq(0).text());
+      // 이미 시간이 그려진 강의이거나, 해당되는 강의 시간이 아니라면 그냥 넘어갑니다.
+      if ($(element).find("span").length > 0 || currentLectureTime !== startTime) {
+        return;
+      }
+      $(element).append(
+        `<span class="time">${lectureStartTime[startTime]} ~ ${lectureEndTime[endTime]}</span>`
+      );
+    });
+        
+  }
 };
 
-export default () => { setTimeout(handleTimeTable, 1000) };
+export default () => { setTimeout(handleTimeTable, 1000); };
